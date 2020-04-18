9d853c13f1bb32a33c927c42e98fed609a8e8504
Eliezer Salvato <eliezer@portabilis.com.br>
2020-04-18 15:22:09 -0300

    Comment devise sign in by login until add css framework

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,15 +17,15 @@ class User < ApplicationRecord
     "#{first_name} #{last_name}".strip
   end
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
 
-    if (login = conditions.delete(:login))
-      where(conditions.to_h).find_by(
-        ['(LOWER(user_name) = :value OR LOWER(email) = :value)', { value: login.downcase }]
-      )
-    elsif conditions.key?(:user_name) || conditions.key?(:email)
-      find_by(conditions.to_h)
-    end
-  end
+  #   if (login = conditions.delete(:login))
+  #     where(conditions.to_h).find_by(
+  #       ['(LOWER(user_name) = :value OR LOWER(email) = :value)', { value: login.downcase }]
+  #     )
+  #   elsif conditions.key?(:user_name) || conditions.key?(:email)
+  #     find_by(conditions.to_h)
+  #   end
+  # end
 end
