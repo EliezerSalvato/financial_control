0b162a49e76be56c69ec2ff1be2a3b428bd9493a
Eliezer Salvato <eliezer@portabilis.com.br>
2020-04-21 17:01:31 -0300

    Add lib axios with rails configuration

---
+++ app/javascript/packs/services.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const token = document.getElementsByName("csrf-token")[0].getAttribute("content");
+
+const headers = {
+  "X-Custom-Header": "foobar",
+  "Accept": "application/json",
+  "Content-Type": "application/json",
+  "X-CSRF-Token": token,
+}
+
+const axiosInstance = axios.create({
+  baseURL: "/api/v1/",
+  timeout: 3000,
+  headers: headers
+});
+
+export const api = {
+  get(endpoint) {
+    return axiosInstance.get(endpoint);
+  },
+  post(endpoint, body) {
+    return axiosInstance.post(endpoint, body);
+  },
+  delete(endpoint) {
+    return axiosInstance.delete(endpoint);
+  }
+};
