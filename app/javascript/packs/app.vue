a966ffb603b4007a0bdc289f29a52eefed1b62a6
Eliezer Salvato <eliezer@portabilis.com.br>
2020-04-26 16:07:16 -0300

    Add transform css transition to change routes prettier

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -21,4 +21,19 @@
 </script>
 
 <style>
+  .v-enter, .v-leave-to {
+    opacity: 0;
+  }
+
+  .v-enter {
+    transform: translate3d(0, -20px, 0);
+  }
+
+  .v-leave-to {
+    transform: translate3d(0, 20px, 0);
+  }
+
+  .v-enter-active, .v-leave-active {
+    transition: all 0.3s;
+  }
 </style>
