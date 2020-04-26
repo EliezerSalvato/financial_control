6ee54fe0cbf60fabe93f96d7a99391b260d4322e
Eliezer Salvato <eliezer@portabilis.com.br>
2020-04-26 16:05:20 -0300

    Add default scope by filter tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  has_scope :page, default: 1
+
+  has_scope :filter, type: :default do |_controller, scope, values|
+    JSON.parse(values).each do |filter_name, filter_value|
+      scope = scope.send(filter_name, filter_value) if filter_value.present?
+    end
+
+    scope
+  end
+
   def configure_permitted_parameters
     added_attrs = [:user_name, :email, :first_name, :last_name, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
