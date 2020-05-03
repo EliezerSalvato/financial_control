da331050e13d89120e6c1bb68cee26ae7060784e
Eliezer Salvato <eliezer@portabilis.com.br>
2020-05-03 13:50:56 -0300

    Create component ModalDelete based on component Modal and using vuex

---
+++ app/javascript/packs/components/ModalDelete.vue
@@ -0,0 +1,50 @@
+<template>
+  <Modal
+    title="Confirmation"
+    :openModal="openModalDelete"
+    @close="close"
+  >
+    <template #body>
+      Are you sure you want to remove the {{ modelName }}{{ currentItemName }}?
+    </template>
+    <template #footer>
+      <button class="button is-success" @click="deleteItem">Yes</button>
+      <button class="button" @click="close">No</button>
+    </template>
+  </Modal>
+</template>
+
+<script>
+  import { mapState, mapActions } from "vuex";
+  import Modal from "./Modal.vue";
+
+  export default {
+    name: "ModalDelete",
+    props: {
+      modelName: { type: String, required: true },
+      currentItemId: { type: Number, required: true },
+      itemName: String
+    },
+    components: {
+      Modal
+    },
+    computed: {
+      ...mapState(["openModalDelete"]),
+      currentItemName() {
+        return (this.itemName ? ` "${this.itemName}"` : "");
+      }
+    },
+    methods: {
+      ...mapActions(["setOpenModalDelete"]),
+      close() {
+        this.setOpenModalDelete(false);
+      },
+      deleteItem() {
+        this.$emit("delete:item", this.currentItemId);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- app/javascript/packs/store.js
+++ app/javascript/packs/store.js
@@ -6,16 +6,23 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   strict: true,
   state: {
-    currentMessage: null
+    currentMessage: null,
+    openModalDelete: false
   },
   mutations: {
     UPDATE_CURRENT_MESSAGE(state, message) {
       state.currentMessage = message;
+    },
+    UPDATE_OPEN_DELETE_MODAL(state, open) {
+      state.openModalDelete = open;
     }
   },
   actions: {
     setCurrentMessage(context, message) {
       context.commit("UPDATE_CURRENT_MESSAGE", message);
+    },
+    setOpenModalDelete(context, open) {
+      context.commit("UPDATE_OPEN_DELETE_MODAL", open);
     }
   }
 });
