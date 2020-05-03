8d6ec6c3f2bb2c81a48f7ea83d27290cc3bd8cfd
Eliezer Salvato <eliezer@portabilis.com.br>
2020-05-03 16:41:57 -0300

    Create input component ButtonNew

---
+++ app/javascript/packs/components/inputs/ButtonNew.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link class="button is-primary is-fullwidth" :to="route">
+    <i class="fa fa-plus"></i>
+    <b class="is-hidden-touch">{{ name }}</b>
+  </router-link>
+</template>
+
+<script>
+  export default {
+    name: "ButtonNew",
+    props: {
+      name: String,
+      route: String
+    }
+  }
+</script>
+
+<style scoped>
+  b {
+    margin-left: 5px;
+    text-transform: capitalize;
+  }
+</style>
