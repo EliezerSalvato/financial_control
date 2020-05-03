ec6bd44ab330c84bdc0f538e39600e7cc43cde67
Eliezer Salvato <eliezer@portabilis.com.br>
2020-05-03 13:48:35 -0300

    Create component modal with bulma

---
+++ app/javascript/packs/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="modal" :class="{ 'is-active': openModal }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header v-if="title" class="modal-card-head">
+        <p class="modal-card-title">{{ title }}</p>
+        <button class="delete" @click="close"></button>
+      </header>
+
+      <section class="modal-card-body">
+        <slot name="body" />
+      </section>
+
+      <footer class="modal-card-foot">
+        <slot name="footer" />
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Modal",
+    props: {
+      title: String,
+      openModal: { type: Boolean, default: false }
+    },
+    methods: {
+      close() {
+        this.$emit("close");
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .modal {
+    margin-top: -15%;
+  }
+
+  .modal-card {
+    max-width: calc(100% - 30px);
+  }
+
+  .modal-card-foot {
+    justify-content: flex-end;
+  }
+</style>
