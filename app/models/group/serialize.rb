a6de9bd7c1423c737e7dc788f76895715d138d3a
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-07-08 02:04:30 -0300

    Refactor GET /api/v1/groups

--- app/models/group/serialize.rb
+++ app/models/group/serialize.rb
@@ -14,26 +14,4 @@ class Group::Serialize
       Failure(:invalid_group) { { errors: group.errors.as_json } }
     end
   end
-
-  class PaginatedRelationAsJson < Micro::Case
-    attribute :relation
-
-    validates :relation, kind: ActiveRecord::Relation
-
-    def call!
-      paginatable = {
-        current_page: relation.current_page,
-        prev_page: relation.prev_page,
-        next_page: relation.next_page,
-        size: relation.size,
-        total_count: relation.total_count,
-        total_pages: relation.total_pages,
-        offset_value: relation.offset_value
-      }
-
-      data = relation.map { |group| Group::Serialize.as_json(group) }
-
-      Success(data: { data: data, paginatable: paginatable })
-    end
-  end
 end
